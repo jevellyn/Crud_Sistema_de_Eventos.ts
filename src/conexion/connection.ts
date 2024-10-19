e4b8fb00500a5c245c5af294481642d8f99bdbfa
jevellyn <juliaevellyn29@gmail.com>
2024-10-19 14:55:53 -0300

    first commit

---
+++ src/conexion/connection.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from "fs";
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+    host: "192.168.13.250",
+    port: 3306,
+    user: "admin",
+    password: "4t8zzgssx8uk2s9",
+    multipleStatements: true,
+});
+
+export async function executar_sql(sql: string) {
+    return await new Promise((resolve, reject) => {
+        connection.query(sql, (error, result) => {
+            if (error) {
+                console.log("Erro ao executar sql:", error.message);
+                return reject(error);
+            } else {
+                return resolve(result);
+            }
+        });
+    });
+}
+
+export async function connectDb() {
+    connection.connect();
+
+    const createTablesSQL = readFileSync(
+        "sql/create_tables_sistema_eventos.sql",
+        "utf-8"
+    );
+    const populateTablesSQL = readFileSync(
+        "sql/insert_tables_sistema_eventos.sql",
+        "utf-8"
+    );
+    const createTriggersSQL = readFileSync(
+        "sql/trigger_insert_participantes_evento.sql",
+        "utf-8"
+    );
+
+    await executar_sql(createTablesSQL);
+    await executar_sql(populateTablesSQL);
+    await executar_sql(createTriggersSQL);
+}
+
+export function disconnectDb() {
+    connection.end();
+}
---
+++ src/models/EventosModel.ts
@@ -0,0 +1,65 @@
+import { executar_sql } from "../conexion/connection";
+
+interface IEventosModel {
+    IDEvento?: number;
+    tipoEvento: string;
+    descricao: string;
+    dataEvento: string;
+    limiteParticipantes: number;
+    duracaoEvento: string;
+}
+
+export class EventosModel implements IEventosModel {
+    IDEvento?: number;
+    tipoEvento: string;
+    descricao: string;
+    dataEvento: string;
+    limiteParticipantes: number;
+    duracaoEvento: string;
+
+    constructor(eventoModel: IEventosModel) {
+        Object.assign(this, eventoModel);
+    }
+
+    static async listar_db() {
+        const SQL = "SELECT * FROM EVENTOS";
+        const eventos = await executar_sql(SQL);
+        console.table(eventos);
+    }
+    //eai?
+    static async count() {
+        const sql = "SELECT COUNT(*) AS eventoQtd FROM EVENTOS";
+        const result = await executar_sql(sql);
+        return result;
+    }
+
+    static async criar_db(evento: EventosModel) {
+        const SQL = `INSERT INTO 
+                  EVENTOS(TIPO_EVENTO, DESCRICAO_EVENTO, DATA_EVENTO, LIMITE_PARTICIPANTES, DURACAO_EVENTO) 
+                  VALUES("${evento.tipoEvento}",
+                        "${evento.descricao}",
+                        "${evento.dataEvento}",
+                        ${evento.limiteParticipantes},
+                        "${evento.duracaoEvento}")`;
+
+        await executar_sql(SQL);
+    }
+
+    static async atualizar_db(evento: EventosModel) {
+        const SQL = `UPDATE EVENTOS 
+                  SET TIPO_EVENTO = "${evento.tipoEvento}", 
+                      DESCRICAO_EVENTO = "${evento.descricao}", 
+                      DATA_EVENTO = "${evento.dataEvento}", 
+                      LIMITE_PARTICIPANTES = ${evento.limiteParticipantes}, 
+                      DURACAO_EVENTO = "${evento.duracaoEvento}"
+                  WHERE EVENTO_ID = ${evento.IDEvento}`;
+
+        await executar_sql(SQL);
+    }
+
+    static async deletar_db(IDEvento: number) {
+        const SQL = `DELETE FROM EVENTOS WHERE ${IDEvento}`;
+
+        await executar_sql(SQL);
+    }
+}
